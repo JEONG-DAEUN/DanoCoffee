bc9e8fda4b92a54e07d248dd70aa7313d3c540e2
JEONG-DAEUN <74965898+JEONG-DAEUN@users.noreply.github.com>
2021-11-09 18:23:16 +0900

    Initial commit

---
+++ src/main/java/com/example/danocoffee/DanoCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.danocoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanoCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanoCoffeeApplication.class, args);
+    }
+
+}
